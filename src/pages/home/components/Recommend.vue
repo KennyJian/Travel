917a19a7aa7c3ad16f5df7879e670e3213e58497
BmJian <574337030@qq.com>
2018-08-09 23:53:23 +0800

    add Recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+      <div class="recommend-title">
+        <img class="title-photo" src="http://img1.qunarzz.com/piao/fusion/1711/89/ebc329f16c55bb02.png">
+        猜你喜欢
+      </div>
+      <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+          <img class="item-img" :src="item.imgUrl">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <div class="item-desc">{{item.desc}}</div>
+          <button class="item-button">查看详情</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1807/99/992c99c98928c509a3.water.jpg_200x200_fc4df6ea.jpg',
+        title: '深圳欢乐谷',
+        desc: '★★★★★  20562条评论'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/64/34362835b4a219af8b36803d31a6380f.water.jpg_200x200_bc5c0452.jpg',
+        title: '深圳小梅沙海洋世界',
+        desc: '★★★★★  5921条评论'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1805/6f/6f885ea8b30c67bca3.water.jpg_200x200_21b03a9c.jpg',
+        title: '世界之窗',
+        desc: '★★★★★  26810条评论'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/mixins.styl"
+  .recommend-title
+    margin-top:.2rem
+    line-height:.8rem
+    background:#eee
+    text-indent:.2rem
+    .title-photo
+      width:0.3rem
+      height:0.3rem
+  .item
+    overflow:hidden
+    display:flex
+    height:1.9rem
+    .item-img
+      width:1.7rem
+      height:1.7rem
+      padding:.1rem
+    .item-info
+      flex:1
+      padding:.1rem
+      min-width:0
+      .item-title
+        line-height:.54rem
+        font-size:.32rem
+        ellipsis()
+      .item-desc
+        line-height:.4rem
+        color:#ccc
+        ellipsis()
+      .item-button
+        line-height:.44rem
+        margin-top:.16rem
+        background:#ff9300
+        padding:0 .2rem
+        border-radius: .06rem
+        color:#fff
+</style>
