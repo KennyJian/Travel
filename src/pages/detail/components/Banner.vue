7e2dc885c9925127526f1d3bdb2b61a857e29d66
BmJian <574337030@qq.com>
2018-08-12 13:07:30 +0800

    banner finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,16 +1,38 @@
 <template>
-    <div class="banner">
+  <div>
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1805/ab/ab4fccbc392da994a3.water.jpg_600x330_a90c3fe9.jpg">
       <div class="banner-info">
         <div class="banner-title">观澜山水田园水上乐园</div>
         <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>39</div>
       </div>
     </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleClose"></common-gallary>
+  </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['//img1.qunarzz.com/sight/p0/1502/15/15ca1d0eab6bf46c.water.jpg_600x330_1108fbd3.jpg',
+        '//img1.qunarzz.com/sight/p0/1805/6f/6f885ea8b30c67bca3.water.jpg_600x330_f2f668e4.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleClose () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
